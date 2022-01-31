4c90379a9df3f82b634e77d60b55e87f8ebfca45
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-01-31 07:28:38 +0100

    Move setup configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -3,32 +3,14 @@
 # This file is part of ufl4rom.
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
+"""ufl4rom setup script."""
 
-from setuptools import find_packages, setup
+import site
+import sys
 
-setup(name="ufl4rom",
-      description="A collection of UFL utilities for reduced order modelling codes",
-      long_description="A collection of UFL utilities for reduced order modelling codes",
-      author="Francesco Ballarin (and contributors)",
-      author_email="francesco.ballarin@unicatt.it",
-      version="0.0.dev1",
-      license="GNU Library or Lesser General Public License (LGPL)",
-      url="https://github.com/RBniCS/ufl4rom",
-      classifiers=[
-          "Development Status :: 3 - Alpha",
-          "Intended Audience :: Developers",
-          "Intended Audience :: Science/Research",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
-          "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-          "Topic :: Scientific/Engineering :: Mathematics",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=["fenics-ufl"],
-      tests_require=["mpi4py", "pytest"]
-      )
+import setuptools
+
+# Workaround for https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+setuptools.setup()
